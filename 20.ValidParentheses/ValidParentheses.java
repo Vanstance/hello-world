511ac21dad66b23f2b7479de882862fe2ea46935
Jianwei <jwzhang86@gmail.com>
2018-08-30 19:17:21 +0800

    NEW...

--- 20.ValidParentheses/ValidParentheses.java
+++ 20.ValidParentheses/ValidParentheses.java
@@ -5,6 +5,22 @@
 */
 class ValidParentheses{
     public boolean isValid(String s) {
-        
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+        	if(st.empty()){
+        		st.push(s.charAt(i));
+        	}else if(s.charAt(i) == ')' && st.peek() == '('){
+        		st.pop();
+        	}else if(s.charAt(i) == '[' && st.peek() == ']'){
+        		st.pop();
+        	}else if(s.charAt(i) == '{' && st.peek() == '}'){
+        		st.pop();
+        	}else {
+        		st.push(s.charAt(i));
+        	}
+        }
+
+        return st.empty();
     }
 }
---
+++ Stack01.java
@@ -0,0 +1,17 @@
+import java.util.*;
+ 
+public class Stack01 {
+    public static void main(String[] args) {
+        Stack stack=new Stack();
+        //1.empty()栈是否为空
+        System.out.println(stack.empty());
+        //2.peek()栈顶值    3.进栈push()
+        stack.push(new Integer(1));
+        stack.push("b");
+        System.out.println(stack.peek());
+        //4.pop()出栈
+        stack.pop();
+        System.out.println(stack.peek());
+        
+    }
+}
